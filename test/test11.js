c35b53716616a1f0f94ceead2eab29ff2751422a
Lei Gong <leigong@ca.ibm.com>
2016-03-20 16:48:45 -0400

    [A] 11: destructuring - string

--- test/test11.js
+++ test/test11.js
@@ -6,17 +6,17 @@ import assert from 'assert';
 describe('11. destructuring also works on strings', () => {
   
   it('destructure every character', () => {
-    let a, b, c = 'abc';
+    let [a, b, c] = 'abc';
     assert.deepEqual([a, b, c], ['a', 'b', 'c']);
   });
   
   it('missing characters are undefined', () => {
-    const [a, c] = 'ab';
+    const [a, , c] = 'ab';
     assert.equal(c, void 0);
   });
   
   it('unicode character work too', () => {
-    const [space, coffee] = 'a ☕';
+    const [, space, coffee] = 'a ☕';
     assert.equal(coffee, '\u{2615}');
   });
   
