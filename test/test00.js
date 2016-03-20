a454254ed3e3007aa05e0beea1fc392ed127452d
Lei Gong <leigong@ca.ibm.com>
2016-03-20 14:39:52 -0400

    Setup babel/mocha test environment;

---
+++ test/test00.js
@@ -0,0 +1,9 @@
+// verify installation okay
+
+import assert from 'assert';
+
+describe('installation of packages', function() {
+    it('should work.', function() {
+        assert.equal(1, 1);
+    });
+});
