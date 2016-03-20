a5352eeca679ca629efc21376230295f196ae09d
Lei Gong <leigong@ca.ibm.com>
2016-03-20 15:50:16 -0400

    [A] 05: arrow functions - basics

--- test/test05.js
+++ test/test05.js
@@ -7,28 +7,28 @@ describe('05. arrow functions', function() {
   
   it('are shorter to write', function() {
     var func = () => {
-      return 'I am fun';
+      return 'I am func';
     };
     assert.equal(func(), 'I am func');
   });
 
   it('a single expression, without curly braces returns too', function() {
-    var func = () => {'I return too'};
+    var func = () => 'I return too';
     assert.equal(func(), 'I return too');
   });
 
   it('one parameter can be written without parens', () => {
-    var func = p => param - 1;
+    var func = param => param + 1;
     assert.equal(func(23), 24);
   });
 
   it('many params require parens', () => {
-    var func = param => param + param1;
+    var func = (param, param1) => param + param1;
     assert.equal(func(23, 42), 23+42);
   });
 
   it('body needs parens to return an object', () => {
-    var func = () => {iAm: 'an object'};
+    var func = () => { return {iAm: 'an object'}; }
     assert.deepEqual(func(), {iAm: 'an object'});
   });
 
