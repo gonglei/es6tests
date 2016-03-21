e418d78cdb2bcd4b59e815f23a12540713bbb5e9
Lei Gong <leigong@ca.ibm.com>
2016-03-20 21:07:17 -0400

    [A] 22: class - creation

--- test/test22.js
+++ test/test22.js
@@ -6,21 +6,21 @@ import assert from 'assert';
 describe('22. class creation', () => {
 
   it('is as simple as `class XXX {}`', function() {
-    let TestClass;
+    class TestClass {};
     
     const instance = new TestClass();
     assert.equal(typeof instance, 'object');
   });
 
   it('class is block scoped', () => {
-    class Inside {}
+    class Outside {}
     {class Inside {}}
     assert.equal(typeof Inside, 'undefined');
   });
   
   it('special method is `constructor`', function() {
     class User {
-      constructor(id) {}
+      constructor(id) { this.id = id; }
     }
     
     const user = new User(42);
@@ -29,6 +29,7 @@ describe('22. class creation', () => {
 
   it('defining a method is simple', function() {
     class User {
+      writesTests() { return false; }
     }
     
     const notATester = new User();
@@ -38,7 +39,7 @@ describe('22. class creation', () => {
   it('multiple methods need no commas (opposed to object notation)', function() {
     class User {
       wroteATest() { this.everWroteATest = true; }
-      isLazy() {  }
+      isLazy() { return !this.everWroteATest; }
     }
     
     const tester = new User();
@@ -48,7 +49,7 @@ describe('22. class creation', () => {
   });
 
   it('anonymous class', () => {
-    const classType = typeof {};
+    const classType = typeof class {};
     assert.equal(classType, 'function');
   });
 
