8ed939d714efb678cb7a55e361f7d8f3da167404
Lei Gong <leigong@ca.ibm.com>
2016-04-10 15:03:53 -0400

    [A] 61: modules - import

--- test/test61.js
+++ test/test61.js
@@ -2,6 +2,10 @@
 // To do: make all tests pass, leave the assert lines unchanged!
 
 import assert from 'assert'; // is only here for completeness, `assert` is always imported by default
+import {equal} from 'assert';
+import {deepEqual, notEqual} from 'assert';
+import {equal as myEqual} from 'assert';
+import myAssert from 'assert';
 
 describe('61. use `import` to import functions that have been exported (somewhere else)', function() {
 
